166957f5194f98d50ac9e44833af97cfa6560e7c
Diego Honório <diegohonoriiio@gmail.com>
2026-03-15 18:05:24 -0300

    Refatoração
    Foi feita uma pequena refatoração, foi criado e implementado uma função que contem, o monleypath, a mesma e usada no lugar de criar toda a logica novammente para os demais tests

--- tests/gastos_views_test.py
+++ tests/gastos_views_test.py
@@ -4,18 +4,16 @@ from src.views.gastos_views import *
 
 # ----------------- Teste de coleta de nome do gasto-------------------
 
-def test_nome_gasto_valido(monkeypatch):
+def test_nome_gasto_valido(simular_input):
     # Simula um input válido diretamente
-    monkeypatch.setattr('builtins.input', lambda _: "Aluguel")
+    simular_input(["Aluguel"])
+
     resultado = nome_gasto()
     assert resultado == "Aluguel"
 
-def test_nome_gasto_vazio_ou_muito_longo(monkeypatch):
+def test_nome_gasto_vazio_ou_muito_longo(simular_input):
     # Simula uma sequência de inputs: primeiro vazio, depois muito longo, por fim válido
-    entradas = ["", "A" * 50, "Supermercado"]
-    iter_entradas = iter(entradas)
-    
-    monkeypatch.setattr('builtins.input', lambda _: next(iter_entradas))
+    simular_input(["", "A" * 50, "Supermercado"])
     
     resultado = nome_gasto()
     # Deve retornar apenas o nome válido no final
@@ -23,31 +21,26 @@ def test_nome_gasto_vazio_ou_muito_longo(monkeypatch):
 
 # ----------------- Teste de coleta do valor do gasto-------------------
 
-def test_valor_gasto(monkeypatch):
+def test_valor_gasto(simular_input):
     # Vai simular um input valido de primeira 
-    monkeypatch.setattr('builtins.input', lambda _: "10,50")
+    simular_input(["10.50"])
+
     resultado = valor_gasto()
     assert resultado == Decimal("10.50")
 
 
-def test_valor_gasto_negaivo_ou_zero(monkeypatch):
+def test_valor_gasto_negaivo_ou_zero(simular_input):
     # Simula a senguencia: valor negativo, zero e por fim valido
-    entradas = ["-5", "0", "100,25"]
-    iter_entradas = iter(entradas)
-
-    monkeypatch.setattr("builtins.input", lambda _: next(iter_entradas))
-
+    simular_input(["-5", "0", "100,25"])
+   
     resultado = valor_gasto()
     # o mesmo retorna apenas o valor valido
     assert resultado == Decimal("100.25")
 
 
-def test_valor_gasto_texto_invalido(monkeypatch):
+def test_valor_gasto_texto_invalido(simular_input):
     # Simnula uma seguencia: texto invalido, depois valido.
-    entradas = ["abc", "50,75"]
-    iter_entradas = iter(entradas)
-
-    monkeypatch.setattr("builtins.input", lambda _: next(iter_entradas))
+    simular_input(["abc", "50,75"])
 
     resultado = valor_gasto()
     assert resultado == Decimal("50.75")
@@ -110,5 +103,5 @@ def test_descricao_gasto_descricao_automatica(simular_input):
     resultado = descricao_gasto()
     assert resultado == "Descrição não informada pelo usuario"
 
-# ----------------- Teste de coleta da de data do gasto-------------------------------
+# ----------------- Teste de coleta da data do gasto-------------------------------
 
